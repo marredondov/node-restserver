74b865700734333637945fa2d5d26da58e6d3cc9
Miguel <marredondov@gmail.com>
2020-06-02 12:10:08 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://miguel:pNkzuZjYYlEhPrJ4@cluster0-gvokr.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
